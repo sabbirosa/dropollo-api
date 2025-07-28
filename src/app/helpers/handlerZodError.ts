d8010b0cfc816ac9612261109f291e5e9ae51dc1
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2025-07-28 19:24:59 +0600

    Add error handling middleware and validation utilities; update dependencies

---
+++ src/app/helpers/handlerZodError.ts
@@ -0,0 +1,22 @@
+import { ZodError } from "zod";
+import {
+  TErrorSources,
+  TGenericErrorResponse,
+} from "../interfaces/error.types";
+
+export const handlerZodError = (err: ZodError): TGenericErrorResponse => {
+  const errorSources: TErrorSources[] = [];
+
+  err.issues.forEach((issue) => {
+    errorSources.push({
+      path: issue.path[issue.path.length - 1]?.toString(),
+      message: issue.message,
+    });
+  });
+
+  return {
+    statusCode: 400,
+    message: "Zod Error",
+    errorSources,
+  };
+};
